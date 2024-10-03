87900b970ff839f94b5af488a20a48bd21086bcb
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2024-10-03 12:00:11 -0300

    feat: adicionando as imagens e dados no banco do firebase storage

--- app/src/main/java/com/example/app_delivery/Form_Cadastro.java
+++ app/src/main/java/com/example/app_delivery/Form_Cadastro.java
@@ -1,13 +1,19 @@
 package com.example.app_delivery;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -19,6 +25,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -27,6 +41,9 @@ public class Form_Cadastro extends AppCompatActivity {
     private Button btn_selecionarFoto, btn_cadastrar;
     private EditText edit_nome, edit_email, edit_senha;
     private TextView txt_mensagemError;
+
+    private String usuarioID;
+    private Uri mSelecionarUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +60,8 @@ public class Form_Cadastro extends AppCompatActivity {
         edit_senha.addTextChangedListener(cadastroTextWatcher);
 
         btn_cadastrar.setOnClickListener(this::CadastrarUsuario);
+
+        btn_selecionarFoto.setOnClickListener(view -> SelecionarFotoGaleria());
     }
 
     public void CadastrarUsuario(View view) {
@@ -50,10 +69,11 @@ public class Form_Cadastro extends AppCompatActivity {
         String senha = edit_senha.getText().toString();
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
+                SalvarDadosUsuario();
                 Snackbar snackbar = Snackbar.make(view, "Usuário Cadastrado com Sucesso!", Snackbar.LENGTH_INDEFINITE)
                         .setAction("OK", v -> finish());
                 snackbar.show();
-            }else {
+            } else {
                 String erro;
                 try {
                     throw task.getException();
@@ -61,21 +81,67 @@ public class Form_Cadastro extends AppCompatActivity {
                     erro = "Digite uma Senha Mais Forte!";
                 } catch (FirebaseAuthInvalidCredentialsException e) {
                     erro = "Digite um E-mail Válido!";
-                }catch (FirebaseAuthUserCollisionException e) {
+                } catch (FirebaseAuthUserCollisionException e) {
                     erro = "Esta Conta Já Existe!";
-                }catch (FirebaseNetworkException e) {
+                } catch (FirebaseNetworkException e) {
                     erro = "Sem Conexão com a Internet!";
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     erro = "Erro ao Cadastrar Usuário: " + e.getMessage();
                 }
                 txt_mensagemError.setText(erro);
-//                Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
-//                snackbar.show();
             }
         });
     }
 
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    assert data != null;
+                    mSelecionarUri = data.getData();
+
+                    try {
+                        fotoUsuario.setImageURI(mSelecionarUri);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+    );
+
+    public void SelecionarFotoGaleria() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        activityResultLauncher.launch(intent);
+    }
+
+    public void SalvarDadosUsuario() {
+        String nomeArquivo = UUID.randomUUID().toString();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference("/images/" + nomeArquivo);
+        reference.putFile(mSelecionarUri)
+                .addOnSuccessListener(taskSnapshot -> reference.getDownloadUrl()
+                        .addOnSuccessListener(uri -> {
+                            String foto = uri.toString();
+                            //Iniciar Banco de Dados
+                            String nome = edit_nome.getText().toString();
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+                            Map<String, Object> usuarios = new HashMap<>();
+                            usuarios.put("nome", nome);
+                            usuarios.put("foto", foto);
+
+                            usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                            DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+                            documentReference.set(usuarios).addOnSuccessListener(unused -> {
+                                Log.i("db", "Sucesso ao Salvar Dados");
+                            }).addOnFailureListener(e -> {
+                                Log.i("db_error", "Erro ao Salvar Dados" + e.toString());
+                            });
+                        })
+                        .addOnFailureListener(e -> Log.i("url_img", e.getMessage()))
+                ).addOnFailureListener(e -> Log.i("url_img", e.getMessage()));
+
+    }
 
     public void IniciarComponentes() {
         fotoUsuario = findViewById(R.id.fotoUsuario);
